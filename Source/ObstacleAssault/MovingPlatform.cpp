263d64e099ed77aa23509de7020d8d47cb609f9b
AmyE123 <amy.elliott2002@yahoo.co.uk>
2025-03-31 15:59:10 +0100

    #48 Using Structs in C++

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -15,12 +15,8 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	MyInt = 9;
 
-	APlusB = InputA + InputB;
-
-	FloatAPlusB = FloatInputA + FloatInputB;
+	MyVector.Y = MyX;
 }
 
 // Called every frame
